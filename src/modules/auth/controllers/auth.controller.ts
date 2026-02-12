247677b689874fcd6ef7a41bd2b699dc4a2ce173
Momen <momen.devv@gmail.com>
2026-02-12 17:43:32 +0200

    feat(auth): implement refresh cookie handling and auth response transformation interceptors

--- src/modules/auth/controllers/auth.controller.ts
+++ src/modules/auth/controllers/auth.controller.ts
@@ -6,17 +6,29 @@ import {
   Patch,
   Param,
   Delete,
+  HttpCode,
+  HttpStatus,
+  UseInterceptors,
 } from '@nestjs/common';
 import { AuthService } from '../services/auth.service';
 import { CreateAuthDto } from '../dto/create-auth.dto';
 import { UpdateAuthDto } from '../dto/update-auth.dto';
+import { Throttle } from '@nestjs/throttler';
+import { SetRefreshCookieInterceptor } from '../interceptors/set-refresh-cookie.interceptor';
+import { TransformAuthResponseInterceptor } from '../interceptors/transform-auth-response.interceptor';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @UseInterceptors(
+    TransformAuthResponseInterceptor,
+    SetRefreshCookieInterceptor,
+  )
+  @Throttle({ default: { limit: 5, ttl: 60000 } })
   @Post('sign-up')
-  async signUp(@Body() createAuthDto: CreateAuthDto) {
-    return this.authService.signUp(createAuthDto);
+  @HttpCode(HttpStatus.CREATED)
+  async signUp(@Body() createUserDto: CreateAuthDto) {
+    return await this.authService.signUp(createUserDto);
   }
 }
